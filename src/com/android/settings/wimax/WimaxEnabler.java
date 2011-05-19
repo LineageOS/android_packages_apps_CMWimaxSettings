e0c54d2a580d483e8f00e3ef7cb248f9506c2a84
Scott Brady <sbradymobile@gmail.com>
2011-05-19 11:43:16 -0700

    Better fix for 4G on at boot: CMWimaxSettings portion
    Update: after more testing we don't need to test for the
    setting, we only need to set it so other services know its
    state.  This simply removes the unnecessary settings check.
    It still works without this change, but it might be better
    to do it to clean up the code.
    
    Change-Id: I9c9122762ae04cb30a12fbacf0382850436c5153

--- src/com/android/settings/wimax/WimaxEnabler.java
+++ src/com/android/settings/wimax/WimaxEnabler.java
@@ -155,12 +155,9 @@ public class WimaxEnabler implements Preference.OnPreferenceChangeListener {
                     + getHumanReadableWimaxStatus(wimaxStatus));
         }
 
-	boolean wimaxSavedState = Settings.Secure.getInt(mContext.getContentResolver(),
-            Settings.Secure.WIMAX_ON, 0) == 1;
-
         if (wimaxStatus == WIMAX_ENABLED_STATE_DISABLED
                 || wimaxStatus == WIMAX_ENABLED_STATE_ENABLED) {
-            boolean wimaxEnabled = (wimaxStatus == WIMAX_ENABLED_STATE_ENABLED && wimaxSavedState);
+            boolean wimaxEnabled = (wimaxStatus == WIMAX_ENABLED_STATE_ENABLED);
             mWimaxCheckBoxPref.setChecked(wimaxEnabled);
             mWimaxCheckBoxPref
                     .setSummary(wimaxStatus == WIMAX_ENABLED_STATE_DISABLED ? mOriginalSummary
@@ -169,16 +166,16 @@ public class WimaxEnabler implements Preference.OnPreferenceChangeListener {
             mWimaxCheckBoxPref.setEnabled(isEnabledByDependency());
 
             if (wimaxStatus == WIMAX_ENABLED_STATE_DISABLED) {
-		Settings.Secure.putInt(mContext.getContentResolver(),
+            Settings.Secure.putInt(mContext.getContentResolver(),
                     Settings.Secure.WIMAX_ON, 0);
                 // mWimaxController.disconnectFromDcs();
                 // ConnectivityManager cManager =
                 // (ConnectivityManager)this.mContext.getSystemService("connectivity");
                 // cManager.resetWimaxService();
             }
-	    else if (wimaxStatus == WIMAX_ENABLED_STATE_ENABLED) {
-		Settings.Secure.putInt(mContext.getContentResolver(),
-                    Settings.Secure.WIMAX_ON, 1);
+            else if (wimaxStatus == WIMAX_ENABLED_STATE_ENABLED) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.WIMAX_ON, 1);
             }
 
         } else if (wimaxStatus == WIMAX_ENABLED_STATE_DISABLING
